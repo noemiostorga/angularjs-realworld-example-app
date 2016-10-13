896eb6114b18434eb5f4160a1bfca9dc4479968f
Noemi Ostorga <noemiostorga@gmail.com>
2016-10-12 17:02:35 -0700

    learned how to add a constant

--- src/js/config/app.constants.js
+++ src/js/config/app.constants.js
@@ -2,6 +2,7 @@ const AppConstants = {
   api: 'http://localhost:3000/api',
   jwtKey: 'jwtToken',
   appName: 'Conduit',
+  random: 'balh',
 };
 
 export default AppConstants;
--- src/js/home/home.controller.js
+++ src/js/home/home.controller.js
@@ -3,6 +3,7 @@ class HomeCtrl {
     'ngInject';
 
     this.appName = AppConstants.appName;
+    this.random = AppConstants.random;
 
   }
 
